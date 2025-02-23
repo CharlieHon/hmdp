a114f70e58989ae5585feaa74dd4724aa597c77c
charlie <2295509496@qq.com>
2025-02-23 23:02:47 +0800

    新增添加商户/店铺类型缓存功能

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopService extends IService<Shop> {
 
+    /**
+     * 根据店铺id查询店铺信息
+     * @param id 店铺id
+     * @return 店铺信息
+     */
+    Result queryShopById(Long id);
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,21 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
 
 /**
  * <p>
@@ -15,6 +26,31 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@RequiredArgsConstructor
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result listShopType() {
+        List<String> shopTypeList = stringRedisTemplate.opsForList().range(CACHE_SHOP_TYPE_KEY, 0, -1);
+        if (CollUtil.isNotEmpty(shopTypeList)) {
+            // 缓存中存在商铺类型
+            List<ShopType> shopTypes = shopTypeList.stream()
+                    .map(jsonStr -> JSONUtil.toBean(jsonStr, ShopType.class))
+                    .sorted(Comparator.comparing(ShopType::getSort))
+                    .collect(Collectors.toList());
+            return Result.ok(shopTypes);
+        }
+        // 缓存中不存在
+        List<ShopType> shopTypes = query().orderByAsc("sort").list();
+        if (CollUtil.isEmpty(shopTypes)) {
+            return Result.fail("店铺类型访问异常！");
+        }
+        List<String> jsonStrList = shopTypes.stream()
+                .map(JSONUtil::toJsonStr)
+                .collect(Collectors.toList());
+        stringRedisTemplate.opsForList().rightPushAll(CACHE_SHOP_TYPE_KEY, jsonStrList);
+        return Result.ok(shopTypes);
+    }
 }
