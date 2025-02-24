2e33fe99662b0a4e3296926e3942f3bd6298e790
charlie <2295509496@qq.com>
2025-02-24 17:14:46 +0800

    缓存击穿：逻辑过期封装类

--- src/main/java/com/hmdp/utils/RedisData.java
+++ src/main/java/com/hmdp/utils/RedisData.java
@@ -4,6 +4,10 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * Redis存储数据对象，包含逻辑过期时间
+ * 存储数据类型为Object，使用JSONUtil.toBean反序列化时，会反序列化为JSONObject类型
+ */
 @Data
 public class RedisData {
     private LocalDateTime expireTime;
