ada68b675fdfa83c8b8ce50f2ec9d08fde67c8e6
charlie <2295509496@qq.com>
2025-02-25 20:52:53 +0800

    一人一单：基于redisson实现分布式锁

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -8,9 +8,10 @@ import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.hmdp.utils.RedisIdWorker;
-import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -34,7 +35,15 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
     private final RedisIdWorker redisIdWorker;
 
+    /**
+     * 自定义redis分布式锁
+     */
     private final StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * redisson
+     */
+    private final RedissonClient redissonClient;
 
     @Override
     // @Transactional
@@ -60,6 +69,28 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足");
         }
 
+        // 三：基于redisson分布式锁
+        Long userId = UserHolder.getUser().getId();
+        // 创建锁对象
+        RLock lock = redissonClient.getLock("lock:order:" + userId);
+        // 获取锁
+        boolean isLock = lock.tryLock();
+        // 判断是否获取锁成功
+        if (!isLock) {
+            // 获取锁失败，返回错误或重试
+            return Result.fail("一个人只允许下一单");
+        }
+        try {
+            // 获取代理对象（事务）
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        } finally {
+            // 释放锁
+            lock.unlock();
+        }
+
+        /*
+        // 二：自定义redis分布式锁
         Long userId = UserHolder.getUser().getId();
         // 创建锁对象
         SimpleRedisLock lock = new SimpleRedisLock("order:" + userId, stringRedisTemplate);
@@ -78,8 +109,9 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             // 释放锁
             lock.unlock();
         }
+        */
 
-        // 单体项目使用 synchronized 锁，当集群部署时无法锁住统一jvm内存
+        // 一：单体项目使用 synchronized 锁，当集群部署时无法锁住统一jvm内存
         // /**
         //  * 1. 锁在事务外部，保证事务提交后再释放锁。如果事务在外，先释放锁，事务还未提交，可能出现一人多单问题。
         //  * 2. 事务失效，直接调用 createVoucherOrder() 方法，等价于 this.createVoucherOder() 没有走代理，导致事务是失效
