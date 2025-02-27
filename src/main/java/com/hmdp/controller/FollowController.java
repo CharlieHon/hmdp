38e934571817cb25f36f0e67227eddda9e56a97c
charlie <2295509496@qq.com>
2025-02-27 19:33:36 +0800

    达人探店：关注/取关

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,10 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,7 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-12-22
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/follow")
 public class FollowController {
 
+    private final IFollowService followService;
+
+    /**
+     * 关注或取关
+     * @param followUserId 目标用户id
+     * @param isFollow 关注/取关
+     */
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long followUserId, @PathVariable("isFollow") Boolean isFollow) {
+        return followService.follow(followUserId, isFollow);
+    }
+
+    /**
+     * 判断当前用户是否关注 followUserId 用户
+     * @param followUserId 目标用户id
+     */
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long followUserId) {
+        return followService.isFollow(followUserId);
+    }
 }
